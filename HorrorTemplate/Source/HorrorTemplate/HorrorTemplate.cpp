79726f3c32592f7bda51a1ebb973667047e0678a
Sebastian Hara <haradevelopment@gmail.com>
2026-09-05 19:30:44 +0200

    setup
    project setup

---
+++ HorrorTemplate/Source/HorrorTemplate/HorrorTemplate.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HorrorTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HorrorTemplate, "HorrorTemplate" );
+
+DEFINE_LOG_CATEGORY(LogHorrorTemplate)
---
+++ HorrorTemplate/Source/HorrorTemplate/HorrorTemplateGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HorrorTemplateGameMode.h"
+
+AHorrorTemplateGameMode::AHorrorTemplateGameMode()
+{
+	// stub
+}
